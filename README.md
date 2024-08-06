1c4b067c23805eed88269b76179274eb35ad6953
richerfu <southorange0929@foxmail.com>
2024-08-06 09:36:07 +0800

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+## C++-Validator
+
+This is my side project to learn c++ and c++ toolchains.
+
+### Goals
+- Based with C++17
+- Full test
+- Api align with zod
---
+++ src/validator.h
@@ -0,0 +1,13 @@
+//
+// Created by ranger on 2024/8/6.
+//
+
+#ifndef CV_VALIDATOR_H
+#define CV_VALIDATOR_H
+
+class Validator {
+public:
+private:
+};
+
+#endif //CV_VALIDATOR_H
